b93185fbb576555590757f62c75bbc577539f084
agenciacaen <agenciacaen@gmail.com>
2026-06-10 16:22:49 -0300

    fix: remove Select.Portal para evitar loop removeChild com Dialog

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -67,9 +67,9 @@ SelectScrollDownButton.displayName =
 
 const SelectContent = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Content>
->(({ className, children, position = "popper", ...props }, ref) => (
-  <SelectPrimitive.Portal>
+  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Content> & { portal?: boolean }
+>(({ className, children, position = "popper", portal = false, ...props }, ref) => {
+  const content = (
     <SelectPrimitive.Content
       ref={ref}
       className={cn(
@@ -93,8 +93,9 @@ const SelectContent = React.forwardRef<
       </SelectPrimitive.Viewport>
       <SelectScrollDownButton />
     </SelectPrimitive.Content>
-  </SelectPrimitive.Portal>
-))
+  );
+  return portal ? <SelectPrimitive.Portal>{content}</SelectPrimitive.Portal> : content;
+})
 SelectContent.displayName = SelectPrimitive.Content.displayName
 
 const SelectLabel = React.forwardRef<
